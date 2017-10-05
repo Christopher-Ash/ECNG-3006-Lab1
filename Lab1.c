8e89e9fbba82125c200983c9e0f34de0ce79a81c
chris <chris@192.168.25.188>
2017-10-05 14:29:48 -0400

    Added include

---
+++ Lab1.c
@@ -0,0 +1,18 @@
+/* Lab 1 first file - ID number 815008210 */
+#include <p18f452.h>
+/* Set configuration bits for use with ICD2 */
+#pragma config OSC = HS
+#pragma config WDT = OFF
+#pragma config LVP = OFF
+
+void main (void)
+{
+/* Insert code from pre-lab Q14 to change values on PORTB */
+    int count;
+    TRISB = 0;
+   for(count = 1; count < 16; count++){
+      PORTB = count;
+   }
+   Sleep();
+}
+
---
+++ build/default/production/Lab1.o.d
@@ -0,0 +1,3 @@
+build/default/production/Lab1.o:  \
+	C:/Users/chris/OneDrive\ for\ Business/Electrical\ Engineering/Year\ Three/ECNG\ 3006/Labs/L01_815008210/Lab1_815008210.X/Lab1.c  \
+
